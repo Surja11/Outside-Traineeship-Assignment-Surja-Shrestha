101b25f4f0b3157c88e947084f84e9b345aec551
Surja11 <stha.surja123@gmail.com>
2026-04-09 10:42:14 +0545

    Assignment completed

---
+++ PHP/Class One/index.php
@@ -0,0 +1,63 @@
+<?php
+
+    $name = htmlspecialchars($_POST['name']);
+    $age = htmlspecialchars($_POST['age']);
+    $color = htmlspecialchars($_POST['color']);
+    $hobbies = $_POST['hobbies'];
+
+    if(!$name)
+        {
+            $message = "Name should be entered";
+            echo "<script>alert(' $message ');</script>";
+            exit();
+    }
+    
+   echo "Hello ".$name;
+
+   echo "<br><br>";
+   
+
+   if (gettype($age)!='int'){
+        $message = "Age should be a number";
+            echo "<script>alert(' $message ');</script>";
+            exit();
+   }
+
+   if($age>18){
+    echo $name." You are an adult.<br><br>";
+   }
+   else{
+    echo $name." You are a minor.<br><br>";
+   }
+    if(!$color)
+        {
+            $message = "Color should be entered";
+            echo "<script>alert(' $message ');</script>";
+            exit();
+    }
+   switch($color){
+        case "red":
+            echo "Red is a bold choice!<br><br>";
+            break;
+        case "blue":
+            echo "Blue is calming!<br><br>";
+            break;
+        case "green":
+            echo "Green represents nature.<br><br>";
+            break;
+        default: 
+            echo "That's an interesting choice!<br><br>";
+   };
+
+   echo "The hobbies you selected are:<br><br>";
+
+    foreach ($hobbies as $hobby) {
+        echo $hobby . "<br>";
+    }
+
+    echo "<br>The loop till the year you have lived is:<br><br>";
+
+    for($i=1;$i<=$age;$i++){
+            echo $i."<br>";
+    }
+?>
